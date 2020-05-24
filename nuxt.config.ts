e108e911e6a91aadcb91e8a733963f0d769aa871
MrBBot <me@mrbbot.dev>
2020-05-24 18:56:27 +0100

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,32 @@ export default {
         hid: "description",
         name: "description",
         content: process.env.npm_package_description || ""
-      }
+      },
+      { name: "msapplication-TileColor", content: "#000000" },
+      { name: "theme-color", content: "#000000" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#000000" }
+    ]
   },
   /*
    ** Customize the progress-bar color
